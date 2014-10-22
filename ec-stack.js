b15c1cb420b8f4920f98ae2820f3b79959b66ac9
Halvard Skogsrud <halvard@skogsrud.com>
2014-10-22 20:50:23 +1100

    Started script for creating ElastiCache cluster
    Only single-AZ for now, but will soon be multi-AZ.

---
+++ ec-stack.js
@@ -0,0 +1,45 @@
+var AWS = require('aws-sdk');
+
+var elasticache = new AWS.ElastiCache({
+  "apiVersion": "2014-07-15",
+  "region": "ap-southeast-2"
+});
+
+var randomString = function() {
+  return Math.random().toString(36).slice(12);
+};
+
+var primaryClusterId = "tax-2a-" + randomString();
+
+var paramsPrimary = {
+  "CacheClusterId": primaryClusterId,
+  "CacheNodeType": "cache.t2.micro",
+  "Engine": "redis",
+  "NumCacheNodes": "1",
+  "PreferredAvailabilityZone": "ap-southeast-2a"
+};
+
+elasticache.createCacheCluster(paramsPrimary, function(err, data) {
+  if (err) {
+    console.log(err, err.stack); // an error occurred
+  } else {
+    console.log(data);           // successful response
+    (function pollUntilAvailable() {
+       setTimeout(function(){
+           elasticache.describeCacheClusters({ "CacheClusterId": primaryClusterId }, function(err, data) {
+             if (err) {
+               console.log(err, err.stack); // an error occurred
+             } else {
+               var status = data.CacheClusters[0].CacheClusterStatus;
+               if (status === "available") {
+                  console.log("Cache cluster [" + primaryClusterId +"] is [" + status + "]. Done!");
+               } else {
+                  console.log("Cache cluster status for [" + primaryClusterId + "] is [" + status + "], waiting...");
+                  pollUntilAvailable();
+               }
+             }
+           });
+       }, 10 * 1000);
+    })();
+  }
+});
