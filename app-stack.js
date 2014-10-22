bec8f2372c5c5bad49e49973d8c98d04cf37d4a3
Halvard Skogsrud <halvard@skogsrud.com>
2014-10-22 16:09:15 +1100

    Show status of CF stack creation
    Should be on interval until complete or deleted/rolled back

--- app-stack.js
+++ app-stack.js
@@ -11,10 +11,24 @@ var stack = require('./cf-stack.json');
 var userdata = shell.exec("python create_mime.py cloud-config.yaml:cloud-config userdata-script.sh:x-shellscript", {silent: true}).output;
 stack.Resources.Ec2Instance.Properties.UserData["Fn::Base64"] = userdata;
 
+var stackName = "nodeapp-" + uuid.v4();
+
 cloudformation.createStack({
-  "StackName": "nodeapp-" + uuid.v4(),
+  "StackName": stackName,
   "TemplateBody": JSON.stringify(stack)
 }, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    console.log(data);
+    setTimeout(function() {
+      cloudformation.describeStackEvents({"StackName": stackName}, function(err, data) {
+        if (err) {
+          console.log(err, err.stack);
+        } else {
+          console.log(data.StackEvents[0].ResourceStatus);
+        }
+      });
+    }, 30 * 1000);
+  }
 });
