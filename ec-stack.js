c64dd5dea8c9577b662107c0d9cdef3dbbe621b9
Halvard Skogsrud <halvard@skogsrud.com>
2014-10-23 13:56:32 +1100

    ElastiCache setup with cross-AZ redundancy
    The script now creates a primary Redis cache cluster in AZ
    ap-southeast-2a and a read replica in ap-southeast-2b.
    
    Promisified, but with lots of duplication still.

--- ec-stack.js
+++ ec-stack.js
@@ -1,3 +1,4 @@
+var Promise = require('es6-promise').Promise;
 var AWS = require('aws-sdk');
 
 var elasticache = new AWS.ElastiCache({
@@ -9,9 +10,12 @@ var randomString = function() {
   return Math.random().toString(36).substr(2, 6);
 };
 
-var primaryClusterId = "tax-2a-" + randomString();
+var suffix = randomString();
+var primaryClusterId = "primary-2a-" + suffix;
+var replicationGroupId = "repgroup-" + suffix;
+var readReplicaClusterId = "replica-2b-" + suffix;
 
-var paramsPrimary = {
+var primaryCacheClusterParams = {
   "CacheClusterId": primaryClusterId,
   "CacheNodeType": "cache.t2.micro",
   "Engine": "redis",
@@ -19,27 +23,112 @@ var paramsPrimary = {
   "PreferredAvailabilityZone": "ap-southeast-2a"
 };
 
-elasticache.createCacheCluster(paramsPrimary, function(err, data) {
-  if (err) {
-    console.log(err, err.stack); // an error occurred
-  } else {
-    console.log(data);           // successful response
+var replicationGroupParams = {
+  "PrimaryClusterId": primaryClusterId,
+  "ReplicationGroupDescription": "Replication Group",
+  "ReplicationGroupId": replicationGroupId
+};
+
+var readReplicaClusterParams = {
+  "CacheClusterId": readReplicaClusterId,
+  "PreferredAvailabilityZone": "ap-southeast-2b",
+  "ReplicationGroupId": replicationGroupId
+};
+
+new Promise(function(resolve, reject) {
+  elasticache.createCacheCluster(primaryCacheClusterParams, function(err, data) {
+    if (err) {
+      reject(err);
+    } else {
+      console.log(data);           // successful response
+      resolve(data);
+    }
+  })
+}).then(function(data) {
+  return new Promise(function(resolve, reject) {
     (function pollUntilAvailable() {
-       setTimeout(function(){
-           elasticache.describeCacheClusters({ "CacheClusterId": primaryClusterId }, function(err, data) {
-             if (err) {
-               console.log(err, err.stack); // an error occurred
-             } else {
-               var status = data.CacheClusters[0].CacheClusterStatus;
-               if (status === "available") {
-                  console.log("Cache cluster [" + primaryClusterId +"] is [" + status + "]. Done!");
-               } else {
-                  console.log("Cache cluster status for [" + primaryClusterId + "] is [" + status + "], waiting...");
-                  pollUntilAvailable();
-               }
-             }
-           });
-       }, 10 * 1000);
+      setTimeout(function(){
+        elasticache.describeCacheClusters({ "CacheClusterId": primaryClusterId }, function(err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            var status = data.CacheClusters[0].CacheClusterStatus;
+            if (status === "available") {
+              console.log("Cache cluster [" + primaryClusterId +"] is [" + status + "]");
+              resolve(status);
+            } else {
+              console.log("Cache cluster status for [" + primaryClusterId + "] is [" + status + "], waiting...");
+              pollUntilAvailable();
+            }
+          }
+        });
+      }, 10 * 1000);
     })();
-  }
+  });
+}).then(function(status) {
+  return new Promise(function(resolve, reject) {
+    elasticache.createReplicationGroup(replicationGroupParams, function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(data);
+        resolve(data);
+      }
+    });
+  });
+}).then(function(data) {
+  return new Promise(function(resolve, reject) {
+    (function pollUntilAvailable() {
+      setTimeout(function(){
+        elasticache.describeReplicationGroups({ "ReplicationGroupId": replicationGroupId, "MaxRecords": 20 }, function(err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            var status = data.ReplicationGroups[0].Status;
+            if (status === "available") {
+              console.log("Replication group [" + replicationGroupId +"] is [" + status + "]");
+              resolve(status);
+            } else {
+              console.log("Replication group status for [" + primaryClusterId + "] is [" + status + "], waiting...");
+              pollUntilAvailable();
+            }
+          }
+        });
+      }, 10 * 1000);
+    })();
+  });
+}).then(function(status) {
+  return new Promise(function(resolve, reject) {
+    elasticache.createCacheCluster(readReplicaClusterParams, function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(data);
+        resolve(data);
+      }
+    });
+  });
+}).then(function(data) {
+  return new Promise(function(resolve, reject) {
+    (function pollUntilAvailable() {
+      setTimeout(function(){
+        elasticache.describeCacheClusters({ "CacheClusterId": readReplicaClusterId }, function(err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            var status = data.CacheClusters[0].CacheClusterStatus;
+            if (status === "available") {
+              console.log("Read replica cache cluster [" + readReplicaClusterId +"] is [" + status + "]");
+              resolve(status);
+            } else {
+              console.log("Read replica cache cluster status for [" + readReplicaClusterId + "] is [" + status + "], waiting...");
+              pollUntilAvailable();
+            }
+          }
+        });
+      }, 10 * 1000);
+    })();
+  });
+}).catch(function(err) {
+  console.log(err, err.stack);
 });
